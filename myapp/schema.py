b157d96bee8fd3c7e3073a9abf56dcdc71345c63
Rubencastillo <98979499+Ruben8224@users.noreply.github.com>
2023-12-02 23:19:37 -0600

    Update schema.py

--- myapp/schema.py
+++ myapp/schema.py
@@ -5,15 +5,11 @@ from myapp.models import DataEntry
 class DataEntryType(DjangoObjectType):
     class Meta:
         model = DataEntry
-        prompt = DataEntry
-        result = DataEntry
- 
+
 class Query(graphene.ObjectType):
     all_data_entries = graphene.List(DataEntryType)
     data_entries_by_user = graphene.List(DataEntryType, user=graphene.String(required=True))
     data_entries_by_model = graphene.List(DataEntryType, model=graphene.String(required=True))
-    data_entries_by_prompt = graphene.List(DataEntryType, prompt=graphene.String(required=True))
-    data_entries_by_result = graphene.List(DataEntryType, result=graphene.String(required=True))
 
     def resolve_all_data_entries(self, info):
         return DataEntry.objects.all()
@@ -24,15 +20,12 @@ class Query(graphene.ObjectType):
     def resolve_data_entries_by_model(self, info, model):
         return DataEntry.objects.filter(model=model)
     
-    def resolve_data_entries_by_prompt(self, info, prompt):
-        return DataEntry.objects.filter(prompt=prompt)
-    
-    def resolve_data_entries_by_result(self, info, result):
-        return DataEntry.objects.filter(result=result)
 
 class CreateDataEntry(graphene.Mutation):
     id = graphene.Int()
     user = graphene.String()
+    carrera = graphene.String()
+    matricula = graphene.String()
     model = graphene.String()
     prompt = graphene.String()
     result = graphene.String()
@@ -43,12 +36,14 @@ class CreateDataEntry(graphene.Mutation):
         prompt = graphene.String()
         result = graphene.String()
 
-    def mutate(self, info, user, model, prompt, result):
-        data_entry = DataEntry(user=user, model=model, prompt=prompt, result=result)
+    def mutate(self, info, user, carrera, matricula, model, prompt, result):
+        data_entry = DataEntry(user=user, carrera=carrera, matricula=matricula, model=model, prompt=prompt, result=result)
         data_entry.save()
 
         return CreateDataEntry(
             id=data_entry.id,
+            carrera=data_entry.carrera,
+            matricula=data_entry.matricula,
             user=data_entry.user,
             model=data_entry.model,
             prompt=data_entry.prompt,
